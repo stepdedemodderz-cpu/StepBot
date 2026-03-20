70ff29ddca2fd73ec80f0e8d099e63d739788366
stepdedemodderz-cpu <stepdedemodderz@gmail.com>
2026-03-20 23:13:08 +0100

    Create app/layout.tsx as the root layout component

---
+++ app/layout.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const RootLayout = ({ children }) => {
+    return (
+        <html lang="en">
+            <body>
+                {children}
+            </body>
+        </html>
+    );
+};
+
+export default RootLayout;
